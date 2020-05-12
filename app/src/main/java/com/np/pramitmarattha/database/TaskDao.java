c37cc971c1b602594513128fff6d368d3376f673
pramitmarattha <pramitmarattha5497@gmail.com>
2020-05-12 13:42:46 +0545

    Populating the list

---
+++ app/src/main/java/com/np/pramitmarattha/database/TaskDao.java
@@ -0,0 +1,27 @@
+package com.np.pramitmarattha.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+
+    @Query("select * from task order by priority")
+    List<TaskEntry> loadAllTasks();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertTask(TaskEntry task);
+
+    @Update
+    void update(TaskEntry task);
+
+    @Delete
+    void deleteTask(TaskEntry task);
+
+}
