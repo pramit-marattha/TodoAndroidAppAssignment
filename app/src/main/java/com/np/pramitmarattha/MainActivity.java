f60bf7580c41a0f1d220d0efcbc434b04cd98b6b
pramitmarattha <pramitmarattha5497@gmail.com>
2020-05-13 13:48:25 +0545

    MainActivity View Model

---
+++ app/src/main/java/com/np/pramitmarattha/MainActivity.java
@@ -0,0 +1,109 @@
+package com.np.pramitmarattha;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.np.pramitmarattha.database.AppDatabase;
+import com.np.pramitmarattha.database.TaskEntry;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemClickListener {
+
+    // Constant for logging
+    private static final String TAG = MainActivity.class.getSimpleName();
+    // Member variables for the adapter and RecyclerView
+    private RecyclerView mRecyclerView;
+    private TaskAdapter mAdapter;
+    AppDatabase database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set the RecyclerView to its corresponding view
+        mRecyclerView = findViewById(R.id.recyclerViewTasks);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // Initialize the adapter and attach it to the RecyclerView
+        mAdapter = new TaskAdapter(this, this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(decoration);
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            // Called when a user swipes left or right on a ViewHolder
+            @Override
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                // Here is where you'll implement swipe to delete
+
+                AppDatabase.databaseWriteExecutor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        int position = viewHolder.getAdapterPosition();
+                        TaskEntry task = mAdapter.getTasks().get(position);
+                        database.taskDao().deleteTask(task);
+
+
+                    }
+                });
+            }
+
+        }).attachToRecyclerView(mRecyclerView);
+        FloatingActionButton fabButton = findViewById(R.id.fab);
+
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to start an AddTaskActivity
+                Intent addTaskIntent = new Intent(MainActivity.this, AddEditTaskActivity.class);
+                startActivity(addTaskIntent);
+            }
+        });
+
+        database = AppDatabase.getInstance(getApplicationContext());
+
+        ViewModelSetUp();
+    }
+
+    @Override
+    public void onItemClickListener(int itemId) {
+        // Launch AddTaskActivity adding the itemId as an extra in the intent
+        Intent intent = new Intent(this, AddEditTaskActivity.class);
+        intent.putExtra(AddEditTaskActivity.EXTRA_TASK_ID, itemId);
+        startActivity(intent);
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    private void ViewModelSetUp() {
+       MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        viewModel.getTasks().observe(this, new Observer<List<TaskEntry>>() {
+            @Override
+            public void onChanged(List<TaskEntry> taskEntries) {
+                Log.d(TAG,"Beep!!Boop!! message coming from the viewmodel");
+                mAdapter.setTasks(taskEntries);
+            }
+        });
+
+    }
+}
+
+
